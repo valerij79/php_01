c0ebc00a035faf3c1b7261ff14a65fb2394aedb2
valerij79 <121170646+valerij79@users.noreply.github.com>
2023-02-21 14:18:15 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import './App.css';
-import Bebras from './Components/003/Bebras';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+const animals = [
+        {name: 'Racoon', color: 'skyblue', big: false},
+        {name: 'Fox', color: 'brown', big: false},
+        {name: 'Moose', color: 'yellow', big: true},
+        {name: 'Wolf', color: 'gray', big: false}
+    ];
 
 function App() {
 
@@ -7,9 +14,21 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <Bebras color="skyblue" lt="1px" name="Jurga"/>
-        <Bebras color="orange" lt="3px" name="Lina" />
-        <Bebras color="yellow" lt="-5px" name="Sima"></Bebras>
+              <div className="card">
+                  <div className="card-header">
+                     <h2>Forest Book</h2>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                      {
+                        animals.map((a, i) => 
+                        <li key={i} className={'list-group-item' + (a.big ? ' big' : '')} 
+                        style={{color:a.color}}>
+                            {a.name}
+                        </li>)
+
+                      }                      
+                  </ul>
+              </div>
 
       </header>
     </div>
