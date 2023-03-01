b84e13562059968448fb569a2162abf97f4d7081
valerij79 <121170646+valerij79@users.noreply.github.com>
2023-03-01 18:59:36 +0200

    My data form

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,50 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import './App.scss';
-
-const animals = [
-    { name: 'Racoon', color: 'skyblue', big: false },
-    { name: 'Fox', color: 'brown', big: false },
-    { name: 'Moose', color: 'yellow', big: true },
-    { name: 'Wolf', color: 'gray', big: false }
-];
-
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-    const [h2Color, setH2Color] = useState('crimson');
-    const [count, setCount] = useState(1);
+    const [vardas, setVardas] = useState('');
+    const [pavarde, setPavarde] = useState('');
+    const [bornMetai, setBornMetai] = useState(0);
+    const [metai, setMetai] = useState(0);
+    const [myList, setMyList] = useState([]);
 
-    const [wish, setWish] = useState('*--*');
-    const [size, setSize] = useState(0);
-
-    const clickIt = () => {
-        setH2Color(c => c === 'crimson' ? 'yellow' : 'crimson');
+    const doVardas = e => {
+        setVardas(e.target.value);
     }
 
-
-
-
-    const add1 = () => {
-        setCount(c => c + 1);
+    const doPavarde = e => {
+        setPavarde(e.target.value);
     }
 
-    const doWish = e => {
-        setWish(e.target.value);
+    const doBornMetai = e => {
+        setBornMetai(e.target.value);
     }
 
-    const doWishSize = e => {
-        setSize(e.target.value);
+    const doMetai = e => {
+        setMetai(e.target.value);
+    }
+
+    const addMy = () => {
+        setMyList(w => [...w,
+        {
+            id: uuidv4(),
+            vardas,
+            pavarde,
+            bornMetai,
+            metai,
+        }
+        ]);
+        setVardas('');
+        setPavarde('');
+        setBornMetai(0);
+        setMetai(0);
+    }
+
+    const del = id => {
+        setMyList(w => w.filter(w => id !== w.id));
     }
 
     return (
@@ -42,29 +52,34 @@ function App() {
             <header className="App-header">
                 <div className="card">
                     <div className="card-header">
-                        <h2 style={{ color: h2Color }}>Forest Book (page {count})</h2>
+                        <h2>My data</h2>
                     </div>
-                    <ul className="list-group list-group-flush">
-                        {
-                            animals.map((a, i) =>
-                                <li key={i} className={'list-group-item' + (a.big ? ' big' : '')}
+                    <ul className="list-group list-group-flush"> {
+                            myList.map((a, i) =>
+                                <li key={i} className="list-group-item"
                                     style={{ color: a.color }}>
-                                    {a.name}
+                                    'Aš esu '{a.vardas} {a.pavarde}'. Man yra '<b>{a.metai-a.bornMetai}</b> 'metai(ų).'
                                 </li>)
                         }
                     </ul>
                     <div className="m-3">
-                        <label className="form-label">Enter your wish</label>
-                        <input type="text" className="form-control" onChange={doWish} value={wish} />
+                        <label className="form-label">Enter your name</label>
+                        <input type="text" className="form-control" onChange={doVardas} value={vardas} />
                     </div>
                     <div className="m-3">
-                        <label className="form-label">How Big <i>{size}</i></label>
-                        <input type="range" className="form-control" min="0" max="10" onChange={doWishSize} value={size} />
+                        <label className="form-label">Enter your surname</label>
+                        <input type="text" className="form-control" onChange={doPavarde} value={pavarde} />
                     </div>
-                </div>
-
-                <button type="button" className="btn btn-outline-warning mt-4" onClick={clickIt}>CLICK</button>
-                <button type="button" className="btn btn-outline-danger mt-4" onClick={add1}>+ 1</button>
+                    <div className="m-3">
+                        <label className="form-label">Enter your born year</label>
+                        <input type="text" className="form-control" onChange={doBornMetai} value={bornMetai} />
+                    </div>
+                    <div className="m-3">
+                        <label className="form-label">Enter today's year</label>
+                        <input type="text" className="form-control" onChange={doMetai} value={metai} />
+                    </div>
+                        <button type="button" className="btn btn-outline-danger m-4" onClick={addMy}>add</button>
+                    </div>
             </header>
         </div>
     );
